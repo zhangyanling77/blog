05ac519882f476c4683aa0ade7e5fe79f1f39d3c
wangcaowei <414162938@qq.com>
2017-09-13 17:25:21 +0800

    多对多查询

--- server/controller/utils.js
+++ server/controller/utils.js
@@ -2,36 +2,38 @@
  * @Author: wangcaowei
  * @Date: 2017-09-06 00:19:53
  * @Last Modified by: wangcaowei
- * @Last Modified time: 2017-09-11 18:04:00
+ * @Last Modified time: 2017-09-13 17:17:42
  */
-const path = require('path')
-const sequelize = require('../db/db');
-const articleMod = sequelize.import (path.join(__dirname, '../models/articles.js'));
-const tagsMod = sequelize.import (path.join(__dirname, '../models/tags.js'));
-const articletagrelateMod = sequelize.import (path.join(__dirname, '../models/articletagrelate.js'))
+const path = require("path");
+const sequelize = require("../db/db");
+const articleMod = sequelize.import(
+  path.join(__dirname, "../models/articles.js")
+);
+const tagsMod = sequelize.import(path.join(__dirname, "../models/tags.js"));
 
-exports.getAllArticle = async() => {
-    // articletagrelateMod.belongsTo(articleMod,{foreignKey:'articleid'})
-    articleMod.hasOne(articletagrelateMod);
-    tagsMod.hasOne(articletagrelateMod);
-    const articleList = await articleMod.findAll({
-        include: [
-            {
-                model: articletagrelateMod,
-            },
-            {
-                {
-                    model: articletagrelateMod,
-                    where: {
-                        articleid: 33
-                    }
-                }, 
-            }
-        ],
-        order: [
-            ['updateTime', 'DESC']
-        ]
-    });
-    console.log(articleList)
-    return articleList;
-}
+//获取所有文章列表 
+exports.getAllArticle = async () => {
+  articleMod.belongsToMany(tagsMod, { through: "articletagrelate" });
+  tagsMod.belongsToMany(articleMod, { through: "articletagrelate" });
+  //   多对多的查询
+  const articleList = await articleMod.findAll({
+    attributes: [
+      "id",
+      "title",
+      "content",
+      "createTime",
+      "updateTime",
+      "userid"
+    ],
+    include: [
+      {
+        model: tagsMod,
+        through: {
+          attributes: []
+        }
+      }
+    ],
+    order: [["updateTime", "DESC"]]
+  });
+  return articleList;
+};
