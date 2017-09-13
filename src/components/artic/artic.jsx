45e89d2b03c1a7667e900ca9dc19015f85cae213
wangcaowei <414162938@qq.com>
2017-09-13 17:56:05 +0800

    tag

--- src/components/artic/artic.jsx
+++ src/components/artic/artic.jsx
@@ -1,37 +1,46 @@
-import React, {Component} from "react";
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Tag } from "antd";
 import "./article.scss";
-import '../../style/base.scss'
+import "../../style/base.scss";
 export default class Artic extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.previousLocation = this.props.location
-    }
-
-    componentWillUpdate(nextProps) {
-        //  const { location } = this.props  // 如果 props.location 不是 modal 的话，就把
-        // this.props.location  // 赋值给 previousLocation。  if ( nextProps.history.action
-        // !== 'POP' &&    (!location.state || !location.state.modal)  ) {
-        // this.previousLocation = this.props.location  }
-    }
-    render() {
-        return (
-            <div>
-                <div className="article">
-                    <p class="article-tag"></p>
-                    <p className="article-title blog-overflow-ellipsis">
-                        <Link to={`/article-detail/${this.props.article.id}`}>{this.props.article.title}</Link>
-                    </p>
-                    <div className='article-content'>
-                        <p className="blog-two-overflow-ellipsis" dangerouslySetInnerHTML={{__html:this.props.article.content}}></p>
-                        <div className="article-info blog-flex blog-flex-justify">
-                            <span>2012-2-2</span>
-                            <span>回复</span>
-                        </div>
-                    </div>
-                </div>
+    this.previousLocation = this.props.location;
+  }
+  componentWillUpdate(nextProps) {
+    //  const { location } = this.props  // 如果 props.location 不是 modal 的话，就把
+    // this.props.location  // 赋值给 previousLocation。  if ( nextProps.history.action
+    // !== 'POP' &&    (!location.state || !location.state.modal)  ) {
+    // this.previousLocation = this.props.location  }
+  }
+  render() {
+    let tags = this.props.article.tags.map(tag => (
+      <Tag color="blue" key={tag.id}>
+        <a href="#">{tag.tag}</a>
+      </Tag>
+    ));
+    return (
+      <div>
+        <div className="article">
+          <p className="article-title blog-overflow-ellipsis">
+            <Link to={`/article-detail/${this.props.article.id}`}>
+              {this.props.article.title}
+            </Link>
+          </p>
+          <p class="article-tag">{tags}</p>
+          <div className="article-content">
+            <p
+              className="blog-two-overflow-ellipsis"
+              dangerouslySetInnerHTML={{ __html: this.props.article.content }}
+            />
+            <div className="article-info blog-flex blog-flex-justify">
+              <span>{new Date(this.props.article.createTime).toLocaleDateString().replace(/\//g,'-')}</span>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
