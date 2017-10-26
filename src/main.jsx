05545564920c8c521b34d27c5d7b075011f0a2d4
wangcaowei <414162938@qq.com>
2017-10-26 17:55:35 +0800

    覆盖掉antd原来的normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
  * @Author: wangcaowei
  * @Date: 2017-08-18 13:02:07
  * @Last Modified by: wangcaowei
- * @Last Modified time: 2017-10-19 17:01:16
+ * @Last Modified time: 2017-10-26 17:53:51
  */
 import React, {Component} from "react";
 import {Provider} from 'react-redux';
@@ -11,13 +11,13 @@ import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleWare from 'redux-thunk'
 import reducers from './reducers/index.js';
 import $ from 'jquery';
-import './style/bootstrap/css/bootstrap.min.css'
 import {Layout} from 'antd';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import Rightcontent from "./components/page/content.jsx"
 import NewArticle from './container/newArticle//newArticle.jsx'
 import ArticleDetail from "./container/articleDetail/index.jsx"
 import 'antd/dist/antd.css'
+import './style/bootstrap/css/bootstrap.min.css'
 import './style/base.scss'
 const {Header, Footer, Sider, Content} = Layout;
 // const history = createHistory(); const middleWare =
