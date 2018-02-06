c5c943e9b576775f8f7a368f64547dcf1e0a554b
wangcaowei <414162938@qq.com>
2018-02-06 10:45:56 +0800

    markdown 配置

--- src/config/markdownConfig.js
+++ src/config/markdownConfig.js
@@ -1,25 +1,25 @@
-const Remarkable = require('remarkable');
-const hljs = require('highlight.js') // https://highlightjs.org/
-import '../style/sublimeTheme.scss'
+const Remarkable = require("remarkable");
+const hljs = require("highlight.js"); // https://highlightjs.org/
+import "../style/sublimeTheme.scss";
 
-export default new Remarkable('full', {
-  html:         false,        // Enable HTML tags in source
-  xhtmlOut:     false,        // Use '/' to close single tags (<br />)
-  breaks:       true,        // Convert '\n' in paragraphs into <br>
-  langPrefix:   'language-',  // CSS language prefix for fenced blocks
-  linkify:      true,         // autoconvert URL-like texts to links
-  linkTarget:   '',           // set target to open link in
+export default new Remarkable("full", {
+  html: false, // Enable HTML tags in source
+  xhtmlOut: false, // Use '/' to close single tags (<br />)
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  langPrefix: "language-", // CSS language prefix for fenced blocks
+  linkify: true, // autoconvert URL-like texts to links
+  linkTarget: "", // set target to open link in
 
   // Enable some language-neutral replacements + quotes beautification
-  typographer:  false,
+  typographer: false,
 
   // Double + single quotes replacement pairs, when typographer enabled,
   // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
-  quotes: '“”‘’',
+  quotes: "“”‘’",
 
   // Highlighter function. Should return escaped HTML,
   // or '' if input not changed
-  highlight: function (str, lang) {
+  highlight: function(str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
         return hljs.highlight(lang, str).value;
@@ -30,6 +30,6 @@ export default new Remarkable('full', {
       return hljs.highlightAuto(str).value;
     } catch (__) {}
 
-    return ''; // use external default escaping
+    return ""; // use external default escaping
   }
 });
